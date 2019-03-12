13f443ee2b70af9bc9edf1d08078111fddf857dc
Tony Gaetani <tony.gaetani@gmail.com>
2019-03-11 23:01:58 -0400

    canvas version

--- fregex.js
+++ fregex.js
@@ -1,47 +1,41 @@
-function drawRegexRec(pixels, regex, pixelId, xmin, xmax, ymin, ymax) {
+function drawRegexRec(ctx, regex, pixelId, xmin, xmax, ymin, ymax) {
 	if (xmin === xmax) {
 		const match = regex.test(pixelId);
 		if (match) {
-			pixels[xmin][ymin] = 'y';
+			ctx.fillStyle = '#000000';
+		} else {
+			ctx.fillStyle = '#FFFFFF';
 		}
+		ctx.fillRect(xmin * 5, ymin * 5, 5, 5);
 	} else {
 		const xmid = Math.floor((xmin + xmax) / 2);
 		const ymid = Math.floor((ymin + ymax) / 2);
-		this.drawRegexRec(pixels, regex, pixelId + '1', xmid + 1, xmax, ymin, ymid);
-		this.drawRegexRec(pixels, regex, pixelId + '2', xmin, xmid, ymin, ymid);
-		this.drawRegexRec(pixels, regex, pixelId + '3', xmin, xmid, ymid + 1, ymax);
-		this.drawRegexRec(pixels, regex, pixelId + '4', xmid + 1, xmax, ymid + 1, ymax);
+		this.drawRegexRec(ctx, regex, pixelId + '1', xmid + 1, xmax, ymin, ymid);
+		this.drawRegexRec(ctx, regex, pixelId + '2', xmin, xmid, ymin, ymid);
+		this.drawRegexRec(ctx, regex, pixelId + '3', xmin, xmid, ymid + 1, ymax);
+		this.drawRegexRec(ctx, regex, pixelId + '4', xmid + 1, xmax, ymid + 1, ymax);
 	}
-}
-
-function fregexHtml(regex, size) {
-	const pixels = Array.from(Array(size)).map(() => Array.from(Array(size)).map(() => ''));
-	this.drawRegexRec(pixels, regex, '', 0, size - 1, 0, size - 1);
-	let result = '';
-	for (let y = 0; y < size - 1; y++) {
-		result += '<div>';
-		for (let x = 0; x < size - 1; x++) {
-			const divClass = pixels[x][y];
-			result += `<span class="${divClass || 'n'}"></span>`;
-		}
-		result += '</div>';
-	}
-	return result;
 }
 
 function fregex() {
 	const sliderValue = document.getElementById('slider').value;
+	const size = Math.pow(2, sliderValue);
 	const regex = new RegExp(document.getElementById('regex').value);
-	const html = fregexHtml(regex, Math.pow(2, sliderValue));
+	const canvas = document.getElementById('fregex-canvas');
+	canvas.width = size * 5;
+	canvas.height = size * 5;
+	const ctx = canvas.getContext('2d');
+	ctx.clearRect(0, 0, sliderValue, sliderValue);
+	this.drawRegexRec(ctx, regex, '', 0, size - 1, 0, size - 1);
+	ctx.stroke();
 	document.getElementById('slider-value').textContent = sliderValue;
-	document.getElementById('fregex').innerHTML = html;
 }
 
 function init() {
 	const input = document.getElementById('regex');
 	input.addEventListener('keyup', (event) => {
 		if (event.keyCode === 13) {
-			fregex();			
+			fregex();
 		}
 	});
 	fregex();
