b62a5b6396c4cb8993c96091a77a107a48d3b488
Tony Gaetani <tony.gaetani@gmail.com>
2019-03-12 22:11:15 -0500

    multiplier and color

--- fregex.js
+++ fregex.js
@@ -1,19 +1,20 @@
-function drawRegexRec(ctx, regex, pixelId, xmin, xmax, ymin, ymax) {
+const drawRegexRec = (ctx, regex, multiplier) => (pixelId, xmin, xmax, ymin, ymax) => {
 	if (xmin === xmax) {
 		const match = regex.test(pixelId);
 		if (match) {
 			ctx.fillStyle = '#000000';
 		} else {
-			ctx.fillStyle = '#FFFFFF';
+			ctx.fillStyle = `#FF${(xmin + ymin) % 99}FF`;
 		}
-		ctx.fillRect(xmin * 5, ymin * 5, 5, 5);
+		ctx.fillRect(xmin * multiplier, ymin * multiplier, 5, 5);
 	} else {
 		const xmid = Math.floor((xmin + xmax) / 2);
 		const ymid = Math.floor((ymin + ymax) / 2);
-		this.drawRegexRec(ctx, regex, pixelId + '1', xmid + 1, xmax, ymin, ymid);
-		this.drawRegexRec(ctx, regex, pixelId + '2', xmin, xmid, ymin, ymid);
-		this.drawRegexRec(ctx, regex, pixelId + '3', xmin, xmid, ymid + 1, ymax);
-		this.drawRegexRec(ctx, regex, pixelId + '4', xmid + 1, xmax, ymid + 1, ymax);
+		const recurse = drawRegexRec(ctx, regex, multiplier);
+		recurse(pixelId + '1', xmid + 1, xmax, ymin, ymid);
+		recurse(pixelId + '2', xmin, xmid, ymin, ymid);
+		recurse(pixelId + '3', xmin, xmid, ymid + 1, ymax);
+		recurse(pixelId + '4', xmid + 1, xmax, ymid + 1, ymax);
 	}
 }
 
@@ -22,11 +23,13 @@ function fregex() {
 	const size = Math.pow(2, sliderValue);
 	const regex = new RegExp(document.getElementById('regex').value);
 	const canvas = document.getElementById('fregex-canvas');
-	canvas.width = size * 5;
-	canvas.height = size * 5;
+	const multiplier = Number(document.getElementById('multiplier').innerText) || 5;
+	canvas.width = size * multiplier;
+	canvas.height = size * multiplier;
 	const ctx = canvas.getContext('2d');
 	ctx.clearRect(0, 0, sliderValue, sliderValue);
-	this.drawRegexRec(ctx, regex, '', 0, size - 1, 0, size - 1);
+	const drawFregex = drawRegexRec(ctx, regex, multiplier);
+	drawFregex('', 0, size - 1, 0, size - 1);
 	ctx.stroke();
 	document.getElementById('slider-value').textContent = sliderValue;
 }
